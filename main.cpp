fd76de73cb793baa09b2c2932a42d44433588d3c
Oscar Francisco López Escobar <36080822+OLopezE@users.noreply.github.com>
2018-12-12 10:52:20 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proebaspalgithub
+//
+//  Created by Oscar Francisco Lopez Escobar on 12/12/18.
+//  Copyright © 2018 Pacorobot. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
